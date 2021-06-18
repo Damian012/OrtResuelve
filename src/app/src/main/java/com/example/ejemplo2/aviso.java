9b9de1e7712ba2d86f05e479754849296cd67d72
Damian012 <damianka26@gmail.com>
2021-06-18 10:34:41 -0300

    primer commit

---
+++ src/app/src/main/java/com/example/ejemplo2/aviso.java
@@ -0,0 +1,41 @@
+package com.example.ejemplo2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.content.Intent;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class aviso extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aviso);
+
+     //   Bundle paqueteRecibido;
+       // paqueteRecibido=this.getIntent().getExtras();
+
+    }
+
+    public void presionarboton(View v){
+        Intent myIntent = new Intent(v.getContext(), misreclamos.class);
+        startActivity(myIntent);
+    }
+}
---
+++ src/app/src/main/java/com/example/ejemplo2/misreclamos.java
@@ -0,0 +1,65 @@
+package com.example.ejemplo2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.content.Intent;
+import android.widget.TextView;
+
+import java.util.Collections;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import static java.util.Objects.requireNonNull;
+
+public class misreclamos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reclamos);
+        // ArrayList<Ubicacion> nuestrasubicaciones;
+       TextView reclamos=findViewById(R.id.reclamos);
+        DatabaseReference mibdd=FirebaseDatabase.getInstance().getReference();
+
+        mibdd.child("Ubicacion");
+        mibdd.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+             Ubicacion miubicacion= snapshot.getValue(Ubicacion.class);
+             String piso=miubicacion.getPiso();
+
+             Log.e("piso:", ""+piso);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+
+
+}
